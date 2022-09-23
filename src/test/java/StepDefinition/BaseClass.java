112dcd0824111bde056a1407159dfb6343261ef7
muskanshrma <muskan.sharma@infostride.com>
2022-09-23 03:20:23 -0700

    Framework

--- src/test/java/StepDefinition/BaseClass.java
+++ src/test/java/StepDefinition/BaseClass.java
@@ -5,7 +5,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
