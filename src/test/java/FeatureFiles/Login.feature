001aaef82c219c3599f3e37d51b7d1b05930b8e8
muskanshrma <muskan.sharma@infostride.com>
2022-09-23 03:47:15 -0700

    tc01

--- src/test/java/FeatureFiles/Login.feature
+++ src/test/java/FeatureFiles/Login.feature
@@ -5,3 +5,11 @@ Feature: Login
     When User enter username and password
     And User click on login button
     Then User is on Inventory page
+
+Feature: Add To cart and checkout
+
+  Scenario: Add item to cart and checkout the product
+    Given User is logged in and clicks on add to cart button
+    When User navigates to shopping cart
+    And User enters details and checksout
+    Then User is on Thank you page
--- src/test/java/StepDefinition/StepDefinition.java
+++ src/test/java/StepDefinition/StepDefinition.java
@@ -10,24 +10,44 @@ import java.io.IOException;
 public class StepDefinition extends BaseClass {
 
     @Given("User is on Login Page")
-    public void login() throws IOException {
+    public void website() throws IOException {
         setup();
     }
 
     @When("User enter username and password")
-    public void checkoutCart() throws IOException {
+    public void loginUser() throws IOException {
 
         pageFactory.getLoginPage().login();
-//        pageFactory.getAddToCartAndCheckout().addToCart();
     }
 
     @And("User click on login button")
-    public void userDetails() throws IOException {
+    public void button() throws IOException {
         pageFactory.getLoginPage().loginButton();
     }
 
     @Then("User is on Inventory page")
     public void user_is_on_inventory_page() throws IOException {
         pageFactory.getLoginPage().verification();
+    }
+
+    @Given("User is logged in and clicks on add to cart button")
+    public void addCart() throws IOException {
+        pageFactory.getAddToCartAndCheckout().addToCart();
+    }
+
+    @When("User navigates to shopping cart")
+    public void shoppingCart() throws IOException {
+
+        pageFactory.getAddToCartAndCheckout().myCart();
+    }
+
+    @And("User enters details and checksout")
+    public void userDetails() throws IOException {
+        pageFactory.getAddToCartAndCheckout().checkoutDetails();
+    }
+
+    @Then("User is on Thank you page")
+    public void user_is_on_thank_you_page() throws IOException {
+        pageFactory.getAddToCartAndCheckout().verifyCheckout();
     }
 }
